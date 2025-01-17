f2e49fb539db26ef71561f39c7b6222278951426
adzashel <seigneur.noir15@gmail.com>
2025-01-17 09:15:23 +0700

    create new collection and connect it to project

--- server.js
+++ server.js
@@ -10,15 +10,25 @@ mongoose.connect('mongodb://localhost:27017/books_data' , {
 }).catch(err => console.error(err));
 
 // create user schema
-
 const userSchema = new mongoose.Schema({
     name: String,
     author: String,
     image : String,
 });
 
+// new user schema
+const newReleaseSchema = new mongoose.Schema({
+    name : String,
+    author: String,
+    image : String,
+    release_date : Date,
+    genre : String,
+    description : String,
+})
+
 // create user model
 const User = mongoose.model('books' , userSchema);
+const NewRelease = mongoose.model('newRelease' , newReleaseSchema);
 
 // export user model
 
